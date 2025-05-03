902b4a84b330f9ea0980da19d53528ab9bcc5413
dkishorkoirala <reyzual.info@gmail.com>
2025-05-03 13:28:29 +0545

    day 1 ends here

---
+++ Python basics/day1/day1_task.py
@@ -0,0 +1,30 @@
+print ("..............introduction.................")
+
+myname = "Dibash Kishor koirala"
+age = 24
+
+print("hi its me", myname , "i am ", age ,"years young.")
+print (".................maths problem..............")
+
+num1 = 345
+num2 = 10
+
+print("addition of the numbers are:", num1+num2)
+print("multiplication of the numbers are:",num1*num2)
+print("reminder of the number is ", num1%num2)
+
+print ("..............age calculator.................")
+
+year_of_birth = int(input("Your year of birth here please:"))
+current_year = 2025
+
+print("Your age is :", current_year-year_of_birth)
+
+print ("................Challenge...............")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age "))
+
+print("Hi", name, "you will be",age+1,"next year!")
+
+print ("...............................")
