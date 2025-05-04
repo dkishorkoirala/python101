6ee2181c272a88e928779b9f3309c4b6448ee0d1
dkishorkoirala <reyzual.info@gmail.com>
2025-05-05 01:32:30 +0545

    Day 2 ends here

---
+++ Python basics/day2/day2_task.py
@@ -0,0 +1,26 @@
+print("..........................................................................................")
+
+username = input("Enter your name: ")
+date_of_birth = int(input("Enter your year of birth: "))
+
+print("Hello", username, ",welcome to the Earth. You are",2025-date_of_birth,"Years old." )
+
+print("..........................................................................................")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("The sum of provided numbers are ", num1 + num2)
+print("The product of the provided numbers are: ", num1*num2)
+print("The average of the two numbers are: ", (num1+num2)/2)
+
+print("................................Mini challenge..........................................................")
+
+name = input("Enter the name: ")
+age = int(input("enter the age: "))
+favourite_number = int(input("Enter your favourite number: "))
+
+print("Hi", name, "You're", age, "years old.")
+print("Your favourite number doubled is", favourite_number*2)
+
+print("..........................................................................................")
