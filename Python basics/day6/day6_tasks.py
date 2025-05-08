e7c63cca1811d8b4275f133458495ca037117677
dkishorkoirala <reyzual.info@gmail.com>
2025-05-09 00:39:10 +0545

    handling list and tuples list have[], tuples are () mutuable and immutuable

---
+++ Python basics/day6/day6_tasks.py
@@ -0,0 +1,23 @@
+print("...........................................................................")
+
+ai_tools= ["chat gpt","leonardo ai","sora ai","any ai","none ai"]
+print(ai_tools[2])
+
+print("...........................................................................")
+
+cities = ("Biratnagar", "Toronto", "new city")
+print("tuples created")
+
+print("...........................................................................")
+
+numbers = []
+
+for i in range(5):
+        n = float(input("Enter a number: "))
+       #i += 1
+        numbers.append(n)
+        
+print("Number entered : ", numbers)
+
+print("Sum of numbers is : ", sum(numbers))
+print("...........................................................................")
